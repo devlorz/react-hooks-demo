950174cbc963337c1399457259ac6688ddea47b0
Nutti Saelor <leelorz6@gmail.com>
2018-11-12 16:24:16 +0700

    add useState

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Counter />
     </div>
   );
 }
