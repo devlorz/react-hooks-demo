6ab00a0bc596b789e605bd9138e9f6294078f08e
Nutti Saelor <leelorz6@gmail.com>
2018-11-15 14:12:21 +0700

    add vanilla tilt by using useEffect hook

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom";
 
 import Counter from "./components/Counter";
 import "./styles.css";
+import Tilt from "./components/VanillaTilt";
+import "./components/VanillaTilt.css";
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <div className="totally-centered">
+        <Tilt>
+          <div className="totally-centered">vanilla-tilt.js</div>
+        </Tilt>
+      </div>
     </div>
   );
 }
